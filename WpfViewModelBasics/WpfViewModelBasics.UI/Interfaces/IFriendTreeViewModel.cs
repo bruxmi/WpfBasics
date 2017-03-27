0f9eebb8f02f631413daae7bd635300ebf698d98
bruxm <bruxm@DESKTOP-BT3K5CG>
2017-03-27 07:41:31 +0200

    FriendTreeView draw

---
+++ WpfViewModelBasics/WpfViewModelBasics.UI/Interfaces/IFriendTreeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using WpfViewModelBasics.UI.ViewModel;
+
+namespace WpfViewModelBasics.UI.Interfaces
+{
+    public interface IFriendTreeViewModel
+    {
+        ObservableCollection<Group> Items { get; }
+    }
+}
---
+++ WpfViewModelBasics/WpfViewModelBasics.UI/ViewModel/FriendTreeViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using WpfViewModelBasics.UI.Interfaces;
+using WpfViewModelBasics.UI.ViewModel.Base;
+
+namespace WpfViewModelBasics.UI.ViewModel
+{
+
+    public class Item : ViewModelBase
+    {
+        public Item()
+        {
+            Items = new ObservableCollection<Item>();
+        }
+
+        public ObservableCollection<Item> Items
+        {
+            get { return GetValue<ObservableCollection<Item>>(); }
+            set { SetValue(value); }
+        }
+
+        public string Name
+        {
+            get { return GetValue<string>(); }
+            set { SetValue(value); }
+        }
+    }
+
+    public class Group : Item
+    {
+
+    }
+
+    public class Entry : Item
+    {
+
+    }
+
+    public class FriendTreeViewModel : ViewModelBase, IFriendTreeViewModel
+    {
+
+
+        public FriendTreeViewModel()
+        {
+        }
+
+        public ObservableCollection<Group> Items
+        {
+            get
+            {
+                var result = new ObservableCollection<Group>();
+                for (int i = 0; i < 5; i++)
+                {
+                    result[i].Name = $"Group {i + 1}";
+                    result[i].Items.Add(
+                    new Group()
+                    {
+                        Name = $"Entry {i + 1}"
+                    });
+                }
+
+                return result;
+            }
+        }
+    }
+}
